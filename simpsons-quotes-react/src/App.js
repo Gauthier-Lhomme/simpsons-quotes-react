168d48f23ba27c9807d350b1db7f1148728f0890
Gauthier <lhomme.gaut@gmail.com>
2022-03-15 17:43:37 +0100

    Create Quotelist and map function

--- simpsons-quotes-react/src/App.js
+++ simpsons-quotes-react/src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import QuoteCard from "./components/QuoteCard";
+import QuoteList from "./components/QuoteList";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <QuoteCard character="Abe Simpson" image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FAbrahamSimpson.png?1497567511593" quote="Why are you pleople avoiding me? Does my withered face remind you of the grim specter of death?"/>
-      <QuoteCard character="Lisa Simpson" image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FLisaSimpson.png?1497567512083" quote="Shut up, brain. I got friends now. I don't need you anymore."/>    
-    </div>
+      <QuoteList/>
+      </div>
   );
 }
 
